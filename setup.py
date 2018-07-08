fb792f9d8b7c2403a653d672172bcbef6f18297d
Robert Clark <robert.d.clark@hpe.com>
2018-07-08 10:46:24 -0500

    Include a setup.py file
    The setup.py file is used to build the package and release it on PyPi
    where it can be easily downloaded and installed using a Python package
    manager, such as PIP. This will allow the software to be easily used in
    production once ready.
    
    Signed-Off-By: Robert Clark <robdclark@outlook.com>

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+with open('VERSION', 'r') as f:
+    version = f.read()
+
+setup(
+    name='sportsreference',
+    version=version,
+    author='Robert Clark',
+    author_email='robdclark@outlook.com',
+    description='A free sports API written for python',
+    long_description=long_description,
+    license='MIT',
+    url='https://github.com/roclark/sportsreference',
+    packages=find_packages(),
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    install_requires=[
+        "pandas >= 0.21.0",
+        "pyquery >= 1.4.0",
+        "requests >= 2.18.4"
+    ],
+    classifiers=(
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+    ),
+)
