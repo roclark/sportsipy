c5a4206ee7bc796182ca7a4147390d2e14736fad
Robert Clark <robert.d.clark@hpe.com>
2018-12-12 09:58:54 -0600

    Allow NBA Boxscores to be iterable
    Instead of requiring users to query boxscores over a single date,
    multiple days should now be supported to allow users to query multiple
    days worth of boxscores from a single call to the Boxscores class. Not
    only will this be easier to use, but this method is also more performant
    than looping over multiple calls to the Boxscores class.
    
    Signed-Off-By: Robert Clark <robdclark@outlook.com>

--- tests/integration/boxscore/test_nba_boxscore.py
+++ tests/integration/boxscore/test_nba_boxscore.py
@@ -28,7 +28,9 @@ def mock_pyquery(url):
             self.text = html_contents
 
     if url == BOXSCORES_URL % (2, 4, YEAR):
-        return MockPQ(read_file('boxscores.html'))
+        return MockPQ(read_file('boxscores-2-4-2017.html'))
+    if url == BOXSCORES_URL % (2, 5, YEAR):
+        return MockPQ(read_file('boxscores-2-5-2017.html'))
     boxscore = read_file('%s.html' % BOXSCORE)
     return MockPQ(boxscore)
 
@@ -164,10 +166,9 @@ class TestNBABoxscore:
 
 
 class TestNBABoxscores:
-    @mock.patch('requests.get', side_effect=mock_pyquery)
-    def test_boxscores_search(self, *args, **kwargs):
-        expected = {
-            'boxscores': [
+    def setup_method(self):
+        self.expected = {
+            '2-4-2017': [
                 {'home_name': 'Atlanta',
                  'home_abbr': 'ATL',
                  'home_score': 113,
@@ -281,6 +282,169 @@ class TestNBABoxscores:
             ]
         }
 
+    @mock.patch('requests.get', side_effect=mock_pyquery)
+    def test_boxscores_search(self, *args, **kwargs):
         result = Boxscores(datetime(2017, 2, 4)).games
+
+        assert result == self.expected
+
+    @mock.patch('requests.get', side_effect=mock_pyquery)
+    def test_boxscores_search_invalid_end(self, *args, **kwargs):
+        result = Boxscores(datetime(2017, 2, 4), datetime(2017, 2, 3)).games
+
+        assert result == self.expected
+
+    @mock.patch('requests.get', side_effect=mock_pyquery)
+    def test_boxscores_search_multiple_days(self, *args, **kwargs):
+        expected = {
+            '2-4-2017': [
+                {'boxscore': '201702040ATL',
+                 'away_name': 'Orlando',
+                 'away_abbr': 'ORL',
+                 'away_score': 86,
+                 'home_name': 'Atlanta',
+                 'home_abbr': 'ATL',
+                 'home_score': 113,
+                 'winning_name': 'Atlanta',
+                 'winning_abbr': 'ATL',
+                 'losing_name': 'Orlando',
+                 'losing_abbr': 'ORL'},
+                {'boxscore': '201702040IND',
+                 'away_name': 'Detroit',
+                 'away_abbr': 'DET',
+                 'away_score': 84,
+                 'home_name': 'Indiana',
+                 'home_abbr': 'IND',
+                 'home_score': 105,
+                 'winning_name': 'Indiana',
+                 'winning_abbr': 'IND',
+                 'losing_name': 'Detroit',
+                 'losing_abbr': 'DET'},
+                {'boxscore': '201702040MIA',
+                 'away_name': 'Philadelphia',
+                 'away_abbr': 'PHI',
+                 'away_score': 102,
+                 'home_name': 'Miami',
+                 'home_abbr': 'MIA',
+                 'home_score': 125,
+                 'winning_name': 'Miami',
+                 'winning_abbr': 'MIA',
+                 'losing_name': 'Philadelphia',
+                 'losing_abbr': 'PHI'},
+                {'boxscore': '201702040MIN',
+                 'away_name': 'Memphis',
+                 'away_abbr': 'MEM',
+                 'away_score': 107,
+                 'home_name': 'Minnesota',
+                 'home_abbr': 'MIN',
+                 'home_score': 99,
+                 'winning_name': 'Memphis',
+                 'winning_abbr': 'MEM',
+                 'losing_name': 'Minnesota',
+                 'losing_abbr': 'MIN'},
+                {'boxscore': '201702040NYK',
+                 'away_name': 'Cleveland',
+                 'away_abbr': 'CLE',
+                 'away_score': 111,
+                 'home_name': 'New York',
+                 'home_abbr': 'NYK',
+                 'home_score': 104,
+                 'winning_name': 'Cleveland',
+                 'winning_abbr': 'CLE',
+                 'losing_name': 'New York',
+                 'losing_abbr': 'NYK'},
+                {'boxscore': '201702040PHO',
+                 'away_name': 'Milwaukee',
+                 'away_abbr': 'MIL',
+                 'away_score': 137,
+                 'home_name': 'Phoenix',
+                 'home_abbr': 'PHO',
+                 'home_score': 112,
+                 'winning_name': 'Milwaukee',
+                 'winning_abbr': 'MIL',
+                 'losing_name': 'Phoenix',
+                 'losing_abbr': 'PHO'},
+                {'boxscore': '201702040SAC',
+                 'away_name': 'Golden State',
+                 'away_abbr': 'GSW',
+                 'away_score': 106,
+                 'home_name': 'Sacramento',
+                 'home_abbr': 'SAC',
+                 'home_score': 109,
+                 'winning_name': 'Sacramento',
+                 'winning_abbr': 'SAC',
+                 'losing_name': 'Golden State',
+                 'losing_abbr': 'GSW'},
+                {'boxscore': '201702040SAS',
+                 'away_name': 'Denver',
+                 'away_abbr': 'DEN',
+                 'away_score': 97,
+                 'home_name': 'San Antonio',
+                 'home_abbr': 'SAS',
+                 'home_score': 121,
+                 'winning_name': 'San Antonio',
+                 'winning_abbr': 'SAS',
+                 'losing_name': 'Denver',
+                 'losing_abbr': 'DEN'},
+                {'boxscore': '201702040UTA',
+                 'away_name': 'Charlotte',
+                 'away_abbr': 'CHO',
+                 'away_score': 98,
+                 'home_name': 'Utah',
+                 'home_abbr': 'UTA',
+                 'home_score': 105,
+                 'winning_name': 'Utah',
+                 'winning_abbr': 'UTA',
+                 'losing_name': 'Charlotte',
+                 'losing_abbr': 'CHO'},
+                {'boxscore': '201702040WAS',
+                 'away_name': 'New Orleans',
+                 'away_abbr': 'NOP',
+                 'away_score': 91,
+                 'home_name': 'Washington',
+                 'home_abbr': 'WAS',
+                 'home_score': 105,
+                 'winning_name': 'Washington',
+                 'winning_abbr': 'WAS',
+                 'losing_name': 'New Orleans',
+                 'losing_abbr': 'NOP'}
+            ],
+            '2-5-2017': [
+                {'boxscore': '201702050BOS',
+                 'away_name': 'LA Clippers',
+                 'away_abbr': 'LAC',
+                 'away_score': 102,
+                 'home_name': 'Boston',
+                 'home_abbr': 'BOS',
+                 'home_score': 107,
+                 'winning_name': 'Boston',
+                 'winning_abbr': 'BOS',
+                 'losing_name': 'LA Clippers',
+                 'losing_abbr': 'LAC'},
+                {'boxscore': '201702050BRK',
+                 'away_name': 'Toronto',
+                 'away_abbr': 'TOR',
+                 'away_score': 103,
+                 'home_name': 'Brooklyn',
+                 'home_abbr': 'BRK',
+                 'home_score': 95,
+                 'winning_name': 'Toronto',
+                 'winning_abbr': 'TOR',
+                 'losing_name': 'Brooklyn',
+                 'losing_abbr': 'BRK'},
+                {'boxscore': '201702050OKC',
+                 'away_name': 'Portland',
+                 'away_abbr': 'POR',
+                 'away_score': 99,
+                 'home_name': 'Oklahoma City',
+                 'home_abbr': 'OKC',
+                 'home_score': 105,
+                 'winning_name': 'Oklahoma City',
+                 'winning_abbr': 'OKC',
+                 'losing_name': 'Portland',
+                 'losing_abbr': 'POR'}
+            ]
+        }
+        result = Boxscores(datetime(2017, 2, 4), datetime(2017, 2, 5)).games
 
         assert result == expected
