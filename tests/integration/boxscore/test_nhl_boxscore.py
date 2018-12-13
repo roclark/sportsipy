0857bdf8a0eeacddb5fb5354bba918a37fbf4cf6
Robert Clark <robert.d.clark@hpe.com>
2018-12-13 14:37:49 -0600

    Allow NHL Boxscores to be iterable
    Instead of requiring users to query boxscores over a single date,
    multiple days should now be supported to allow users to query multiple
    days worth of boxscores from a single call to the Boxscores class. Not
    only will this be easier to use, but this method is also more performant
    than looping over multiple calls to the Boxscores class.
    
    Signed-Off-By: Robert Clark <robdclark@outlook.com>

--- tests/integration/boxscore/test_nhl_boxscore.py
+++ tests/integration/boxscore/test_nhl_boxscore.py
@@ -28,7 +28,9 @@ def mock_pyquery(url):
             self.text = html_contents
 
     if url == BOXSCORES_URL % (2, 4, YEAR):
-        return MockPQ(read_file('boxscores.html'))
+        return MockPQ(read_file('boxscores-2-4-2017.html'))
+    if url == BOXSCORES_URL % (2, 5, YEAR):
+        return MockPQ(read_file('boxscores-2-5-2017.html'))
     boxscore = read_file('%s.html' % BOXSCORE)
     return MockPQ(boxscore)
 
@@ -125,10 +127,9 @@ class TestNHLBoxscore:
 
 
 class TestNHLBoxscores:
-    @mock.patch('requests.get', side_effect=mock_pyquery)
-    def test_boxscores_search(self, *args, **kwargs):
-        expected = {
-            'boxscores': [
+    def setup_method(self):
+        self.expected = {
+            '2-4-2017': [
                 {'boxscore': '201702040BOS',
                  'away_name': 'Toronto Maple Leafs',
                  'away_abbr': 'TOR',
@@ -275,6 +276,202 @@ class TestNHLBoxscores:
             ]
         }
 
+    @mock.patch('requests.get', side_effect=mock_pyquery)
+    def test_boxscores_search(self, *args, **kwargs):
         result = Boxscores(datetime(2017, 2, 4)).games
+
+        assert result == self.expected
+
+    @mock.patch('requests.get', side_effect=mock_pyquery)
+    def test_boxscores_search_invalid_end(self, *args, **kwargs):
+        result = Boxscores(datetime(2017, 2, 4), datetime(2017, 2, 3)).games
+
+        assert result == self.expected
+
+    @mock.patch('requests.get', side_effect=mock_pyquery)
+    def test_boxscores_search_multiple_days(self, *args, **kwargs):
+        expected = {
+            '2-4-2017': [
+                {'boxscore': '201702040BOS',
+                 'away_name': 'Toronto Maple Leafs',
+                 'away_abbr': 'TOR',
+                 'away_score': 6,
+                 'home_name': 'Boston Bruins',
+                 'home_abbr': 'BOS',
+                 'home_score': 5,
+                 'winning_name': 'Toronto Maple Leafs',
+                 'winning_abbr': 'TOR',
+                 'losing_name': 'Boston Bruins',
+                 'losing_abbr': 'BOS'},
+                {'boxscore': '201702040BUF',
+                 'away_name': 'Ottawa Senators',
+                 'away_abbr': 'OTT',
+                 'away_score': 0,
+                 'home_name': 'Buffalo Sabres',
+                 'home_abbr': 'BUF',
+                 'home_score': 4,
+                 'winning_name': 'Buffalo Sabres',
+                 'winning_abbr': 'BUF',
+                 'losing_name': 'Ottawa Senators',
+                 'losing_abbr': 'OTT'},
+                {'boxscore': '201702040CBJ',
+                 'away_name': 'New Jersey Devils',
+                 'away_abbr': 'NJD',
+                 'away_score': 5,
+                 'home_name': 'Columbus Blue Jackets',
+                 'home_abbr': 'CBJ',
+                 'home_score': 1,
+                 'winning_name': 'New Jersey Devils',
+                 'winning_abbr': 'NJD',
+                 'losing_name': 'Columbus Blue Jackets',
+                 'losing_abbr': 'CBJ'},
+                {'boxscore': '201702040COL',
+                 'away_name': 'Winnipeg Jets',
+                 'away_abbr': 'WPG',
+                 'away_score': 2,
+                 'home_name': 'Colorado Avalanche',
+                 'home_abbr': 'COL',
+                 'home_score': 5,
+                 'winning_name': 'Colorado Avalanche',
+                 'winning_abbr': 'COL',
+                 'losing_name': 'Winnipeg Jets',
+                 'losing_abbr': 'WPG'},
+                {'boxscore': '201702040DAL',
+                 'away_name': 'Chicago Blackhawks',
+                 'away_abbr': 'CHI',
+                 'away_score': 5,
+                 'home_name': 'Dallas Stars',
+                 'home_abbr': 'DAL',
+                 'home_score': 3,
+                 'winning_name': 'Chicago Blackhawks',
+                 'winning_abbr': 'CHI',
+                 'losing_name': 'Dallas Stars',
+                 'losing_abbr': 'DAL'},
+                {'boxscore': '201702040MTL',
+                 'away_name': 'Washington Capitals',
+                 'away_abbr': 'WSH',
+                 'away_score': 3,
+                 'home_name': 'Montreal Canadiens',
+                 'home_abbr': 'MTL',
+                 'home_score': 2,
+                 'winning_name': 'Washington Capitals',
+                 'winning_abbr': 'WSH',
+                 'losing_name': 'Montreal Canadiens',
+                 'losing_abbr': 'MTL'},
+                {'boxscore': '201702040NSH',
+                 'away_name': 'Detroit Red Wings',
+                 'away_abbr': 'DET',
+                 'away_score': 1,
+                 'home_name': 'Nashville Predators',
+                 'home_abbr': 'NSH',
+                 'home_score': 0,
+                 'winning_name': 'Detroit Red Wings',
+                 'winning_abbr': 'DET',
+                 'losing_name': 'Nashville Predators',
+                 'losing_abbr': 'NSH'},
+                {'boxscore': '201702040NYI',
+                 'away_name': 'Carolina Hurricanes',
+                 'away_abbr': 'CAR',
+                 'away_score': 5,
+                 'home_name': 'New York Islanders',
+                 'home_abbr': 'NYI',
+                 'home_score': 4,
+                 'winning_name': 'Carolina Hurricanes',
+                 'winning_abbr': 'CAR',
+                 'losing_name': 'New York Islanders',
+                 'losing_abbr': 'NYI'},
+                {'boxscore': '201702040PHI',
+                 'away_name': 'Los Angeles Kings',
+                 'away_abbr': 'LAK',
+                 'away_score': 1,
+                 'home_name': 'Philadelphia Flyers',
+                 'home_abbr': 'PHI',
+                 'home_score': 0,
+                 'winning_name': 'Los Angeles Kings',
+                 'winning_abbr': 'LAK',
+                 'losing_name': 'Philadelphia Flyers',
+                 'losing_abbr': 'PHI'},
+                {'boxscore': '201702040SJS',
+                 'away_name': 'Arizona Coyotes',
+                 'away_abbr': 'ARI',
+                 'away_score': 3,
+                 'home_name': 'San Jose Sharks',
+                 'home_abbr': 'SJS',
+                 'home_score': 2,
+                 'winning_name': 'Arizona Coyotes',
+                 'winning_abbr': 'ARI',
+                 'losing_name': 'San Jose Sharks',
+                 'losing_abbr': 'SJS'},
+                {'boxscore': '201702040STL',
+                 'away_name': 'Pittsburgh Penguins',
+                 'away_abbr': 'PIT',
+                 'away_score': 4,
+                 'home_name': 'St. Louis Blues',
+                 'home_abbr': 'STL',
+                 'home_score': 1,
+                 'winning_name': 'Pittsburgh Penguins',
+                 'winning_abbr': 'PIT',
+                 'losing_name': 'St. Louis Blues',
+                 'losing_abbr': 'STL'},
+                {'boxscore': '201702040TBL',
+                 'away_name': 'Anaheim Ducks',
+                 'away_abbr': 'ANA',
+                 'away_score': 2,
+                 'home_name': 'Tampa Bay Lightning',
+                 'home_abbr': 'TBL',
+                 'home_score': 3,
+                 'winning_name': 'Tampa Bay Lightning',
+                 'winning_abbr': 'TBL',
+                 'losing_name': 'Anaheim Ducks',
+                 'losing_abbr': 'ANA'},
+                {'boxscore': '201702040VAN',
+                 'away_name': 'Minnesota Wild',
+                 'away_abbr': 'MIN',
+                 'away_score': 6,
+                 'home_name': 'Vancouver Canucks',
+                 'home_abbr': 'VAN',
+                 'home_score': 3,
+                 'winning_name': 'Minnesota Wild',
+                 'winning_abbr': 'MIN',
+                 'losing_name': 'Vancouver Canucks',
+                 'losing_abbr': 'VAN'}
+            ],
+            '2-5-2017': [
+                {'boxscore': '201702050MTL',
+                 'away_name': 'Edmonton Oilers',
+                 'away_abbr': 'EDM',
+                 'away_score': 1,
+                 'home_name': 'Montreal Canadiens',
+                 'home_abbr': 'MTL',
+                 'home_score': 0,
+                 'winning_name': 'Edmonton Oilers',
+                 'winning_abbr': 'EDM',
+                 'losing_name': 'Montreal Canadiens',
+                 'losing_abbr': 'MTL'},
+                {'boxscore': '201702050NYR',
+                 'away_name': 'Calgary Flames',
+                 'away_abbr': 'CGY',
+                 'away_score': 3,
+                 'home_name': 'New York Rangers',
+                 'home_abbr': 'NYR',
+                 'home_score': 4,
+                 'winning_name': 'New York Rangers',
+                 'winning_abbr': 'NYR',
+                 'losing_name': 'Calgary Flames',
+                 'losing_abbr': 'CGY'},
+                {'boxscore': '201702050WSH',
+                 'away_name': 'Los Angeles Kings',
+                 'away_abbr': 'LAK',
+                 'away_score': 0,
+                 'home_name': 'Washington Capitals',
+                 'home_abbr': 'WSH',
+                 'home_score': 5,
+                 'winning_name': 'Washington Capitals',
+                 'winning_abbr': 'WSH',
+                 'losing_name': 'Los Angeles Kings',
+                 'losing_abbr': 'LAK'}
+            ]
+        }
+        result = Boxscores(datetime(2017, 2, 4), datetime(2017, 2, 5)).games
 
         assert result == expected
