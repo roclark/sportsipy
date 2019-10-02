4377ec58cf823edfd071373755081801119e4013
Robert Clark <robdclark@outlook.com>
2019-10-02 08:26:27 -0500

    Fix issues running duplicate exhaustive tests
    The exhaustive test suite is currently trying to run duplicate jobs and
    is failing as a result. No duplicates should be run, and the code should
    test all requested platforms and versions.
    
    Signed-Off-By: Robert Clark <robdclark@outlook.com>

--- tests/exhaustive/test_ncaaf.py
+++ tests/exhaustive/test_ncaaf.py
@@ -1,12 +1,14 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(sys.path[0])))
 from sportsreference.ncaaf.conferences import Conferences
 from sportsreference.ncaaf.rankings import Rankings
 from sportsreference.ncaaf.teams import Teams
 
 for team in Teams():
     print(team.name)
-    for player in team.roster:
+    for player in team.roster.players:
         print(player.name)
-    for game in schedule:
+    for game in team.schedule:
         print(game.dataframe)
         print(game.dataframe_extended)
 
