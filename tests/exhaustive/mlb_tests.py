c52cb7e9379b693b44c32bafe349412052019980
Robert Clark <robdclark@outlook.com>
2019-11-18 12:07:14 -0600

    Fix encoding issues with player names in tests
    The exhaustive tests are throwing errors on the Windows tests when it
    tries to parse players that have accents in their names. To prevent the
    errors from being thrown and the tests from failing, this should be
    caught and handled properly.
    
    Signed-Off-By: Robert Clark <robdclark@outlook.com>

--- tests/exhaustive/mlb_tests.py
+++ tests/exhaustive/mlb_tests.py
@@ -5,7 +5,10 @@ from sportsreference.mlb.teams import Teams
 for team in Teams():
     print(team.name)
     for player in team.roster.players:
-        print(player.name)
+        try:
+            print(player.name)
+        except UnicodeEncodeError:
+            print(player.name.encode('utf-8'))
     for game in team.schedule:
         print(game.dataframe)
         print(game.dataframe_extended)
