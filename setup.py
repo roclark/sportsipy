139624aa8ff8f785715c6056069849190da535f7
Himadri Ganguly <himadri@colourdrift.com>
2020-09-30 02:31:13 +0530

    Modified setup.py to fix error 'classifiers' should be a list, got type 'tuple'.

--- setup.py
+++ setup.py
@@ -1,33 +1,29 @@
 from setuptools import find_packages, setup
 
-with open('README.rst', 'r') as f:
+with open("README.rst", "r") as f:
     long_description = f.read()
 
-with open('VERSION', 'r') as f:
+with open("VERSION", "r") as f:
     version = f.read()
 
 setup(
-    name='sportsreference',
+    name="sportsreference",
     version=version,
-    author='Robert Clark',
-    author_email='robdclark@outlook.com',
-    description='A free sports API written for python',
+    author="Robert Clark",
+    author_email="robdclark@outlook.com",
+    description="A free sports API written for python",
     long_description=long_description,
-    license='MIT',
-    url='https://github.com/roclark/sportsreference',
+    license="MIT",
+    url="https://github.com/roclark/sportsreference",
     packages=find_packages(),
-    python_requires='>=3.6',
-    keywords='stats sports api sportsreference machine learning',
-    install_requires=[
-        "pandas >= 0.24.1",
-        "pyquery >= 1.4.0",
-        "requests >= 2.18.4"
+    python_requires=">=3.6",
+    keywords="stats sports api sportsreference machine learning",
+    install_requires=["pandas >= 0.24.1", "pyquery >= 1.4.0", "requests >= 2.18.4"],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
     ],
-    classifiers=(
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX :: Linux',
-    ),
 )
